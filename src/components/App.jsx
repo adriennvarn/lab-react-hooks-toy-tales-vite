e7e1fcf59a27f6ef5100269b53c9842681740132
Adrienn Varn <adriennvarn@gmail.com>
2026-09-14 18:42:35 -0400

    inital load implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,29 @@
 import React, { useState } from "react"
-
 import Header from "./Header"
 import ToyForm from "./ToyForm"
 import ToyContainer from "./ToyContainer"
+import { useEffect } from "react"
+
+const API_URL = "http://localhost:3001/toys"
 
 function App() {
+    // form visibility
     const [showForm, setShowForm] = useState(false)
+    // toys list
+    const [toys, setToys] = useState([])
 
+    // on page load, initialize list
+    useEffect(() => {
+        fetch(API_URL)
+            .then(r => { 
+                if (r.ok) return r.json()
+                else throw new Error("Error fetching toys:", r.status)
+            })
+            .then(setToys)
+            .catch(err => console.error(err))
+    })
+
+    // show form
     function handleClick() {
         setShowForm((showForm) => !showForm)
     }
@@ -18,7 +35,7 @@ function App() {
             <div className="buttonContainer">
                 <button onClick={handleClick}>Add a Toy</button>
             </div>
-            <ToyContainer />
+            <ToyContainer toys={toys} />
         </>
     )
 }
